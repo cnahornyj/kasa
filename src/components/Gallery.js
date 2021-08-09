168c94246238778cfcdf6fd5d423a64b36cd4f60
Clara <c.nahornyj@hotmail.fr>
2021-08-09 17:14:52 +0200

    Ajout du composant Gallery pour la page d'un logement

---
+++ src/components/Gallery.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Carousel extends Component {
+    render() {
+        return (
+            <section className="carousel">
+
+            </section>
+        );
+    }
+}
+
+export default Carousel;
