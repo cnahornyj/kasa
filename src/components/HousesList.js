200e954814f1737b33b8158acd38428a37d8cba5
Clara <c.nahornyj@hotmail.fr>
2021-08-31 19:54:20 +0200

    Stockage de la donnée dans le localStorage

--- src/components/HousesList.js
+++ src/components/HousesList.js
@@ -1,21 +1,38 @@
 import React, { Component } from "react";
 import "../styles/HousesList.css";
-import Data from "../data/data";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class HousesList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      houses: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((result) => {
+        this.setState({
+          houses: result,
+        });
+      });
+  }
+
   render() {
-    // Récupération des données via la variable houses
-    const houses = Data;
-    // Méthode map sur la liste des logements et lien vers leur page
+    const houses = this.state.houses;
+    console.log(this.state.houses);
+    localStorage.setItem("houses",JSON.stringify(this.state.houses));
     return (
       <section className="housesList">
         {houses.map((house, index) => (
           <article key={`${house}-${house.id}`} className="card">
-            <NavLink to={`/house/${house.id}`}>
+            <Link to={`/house/${house.id}`}>
               <img src={house.cover} alt="Logement" className="imgHouse" />
               <p className="legend">{house.title}</p>
-            </NavLink>
+            </Link>
           </article>
         ))}
       </section>
