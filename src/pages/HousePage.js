9900df941717aa9adb4ab5ac947cdc48cb80bbae
Clara <c.nahornyj@hotmail.fr>
2021-08-31 19:55:10 +0200

    Récupération de la data propre au logement via les params

--- src/pages/HousePage.js
+++ src/pages/HousePage.js
@@ -6,7 +6,6 @@ import Rating from "../components/Rating";
 import HostProfile from "../components/HostProfile";
 import HouseDetails from "../components/HouseDetails";
 import TextBloc from "../components/TextBloc";
-import data from "../data/data";
 
 function findIndexByKeyValue(house, key, valuetosearch) {
   for (let i = 0; i < house.length; i++) {
@@ -17,33 +16,26 @@ function findIndexByKeyValue(house, key, valuetosearch) {
   return null;
 }
 
+
 class HousePage extends Component {
+
   render() {
-    
-    // Récupération de la data via la variable houses
-    const houses = data;
-    // Récupération de l'id du logement dans l'URL
-    const idHouse = window.location.href.substring(28);
-    // Récupération de l'index du logement dans le tableau de logements
-    const index = findIndexByKeyValue(houses, "id", idHouse);
-    // Récupération de la data propre au logement grâce à son index via la variable house
+
+    // Récupération de la data stockée dans le localStorage
+    const data = localStorage.getItem("houses");
+    // Conversion JSON en JS
+    const houses = JSON.parse(data);
+    // Récupération de l'id du logement via les paramètres de l'url
+    const params = this.props.match.params.id;
+    // Trouver l'index du logement dans le tableau de logements
+    const index = findIndexByKeyValue(houses, "id", params);
+    // Récupération uniquement des informations propres au logement
     const house = houses[index];
-
-    // Conversion de la note du logement sur /5 en nombre
-    const nbOfStars = parseInt(house.rating) + 1;
-    let arrayOfRatings = [];
-
-    for (let logement = 1; logement < nbOfStars; logement++) {
-      arrayOfRatings.push(logement);
-    }
-
-    console.log(arrayOfRatings);
 
     return (
       <section className="housepage">
-        <Header />
-        <article>
-          {/* Remplissage des props du composant HouseDetails */}
+      <Header />
+        <article className="locAndHost">
           <HouseDetails
             title={house.title}
             location={house.location}
@@ -51,24 +43,20 @@ class HousePage extends Component {
           />
           <article>
             <article>
-              {/* Remplissage des props du composant HostProfile */}
               <HostProfile
                 name={house.host.name}
                 picture={house.host.picture}
               />
             </article>
-            <Rating />
-            <p>{house.rating}</p>
+            <Rating rating={house.rating} />
           </article>
         </article>
         <article>
-          {/* Remplissage des props du composant TextBloc */}
           <TextBloc title="Description" description={house.description} />
-          {/* Remplissage des props du composant TextBloc */}
           <TextBloc
             title="Equipements"
             description={house.equipments.map((equipment, index) => (
-              <p key={`${equipment}-${index}`}>{equipment}</p>
+              <li key={`${equipment}-${index}`}>{equipment}</li>
             ))}
           />
         </article>
