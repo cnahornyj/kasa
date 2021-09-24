b48a81f6b15f9e60396dbf1a309572af1dab9d4c
Clara <c.nahornyj@hotmail.fr>
2021-09-24 15:14:54 +0200

    Modifications des chemins pour les views

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import "../styles/App.css";
 import React, { Component } from "react";
 import {BrowserRouter, Switch, Route } from 'react-router-dom'
-import Home from "../pages/Home";
-import Propos from "../pages/Propos";
-import HousePage from "../pages/HousePage";
-import Error from "../pages/Error"
+import Home from "../views/Home";
+import Propos from "../views/Propos";
+import HousePage from "../views/HousePage";
+import Error from "../views/Error"
 
 class App extends Component {
 
