6cb1d7988f6245460ea20becc4d1b03f6778f091
Clara <c.nahornyj@hotmail.fr>
2021-09-28 16:32:04 +0200

    Récupération des données d'un logement via la méthode componentDidMount

--- src/views/HousePage.js
+++ src/views/HousePage.js
@@ -2,69 +2,84 @@ import React, { Component } from "react";
 import "../styles/HousePage.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Gallery  from "../components/Gallery";
+import Gallery from "../components/Gallery";
 import Rating from "../components/Rating";
 import HostProfile from "../components/HostProfile";
 import HouseDetails from "../components/HouseDetails";
 import Collapse from "../components/Collapse";
 
-function findIndexByKeyValue(house, key, valuetosearch) {
-  for (let i = 0; i < house.length; i++) {
-    if (house[i][key] === valuetosearch) {
-      return i;
-    }
-  }
-  return null;
-}
-
-
 class HousePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      house: null,
+    };
+  }
+
+  findIndexByKeyValue(house, key, valuetosearch) {
+    for (let i = 0; i < house.length; i++) {
+      if (house[i][key] === valuetosearch) {
+        return i;
+      }
+    }
+    return null;
+  }
+
+  componentDidMount() {
+    const data = localStorage.getItem("houses");
+    const params = this.props.match.params.id;
+    const houses = JSON.parse(data);
+    const index = this.findIndexByKeyValue(houses, "id", params);
+    const apartment = houses[index];
+    this.setState({
+      house: apartment,
+    });
+  }
 
   render() {
-
-    // Récupération de la data stockée dans le localStorage
-    const data = localStorage.getItem("houses");
-    // Conversion JSON en JS
-    const houses = JSON.parse(data);
-    // Récupération de l'id du logement via les paramètres de l'url
-    const params = this.props.match.params.id;
-    // Trouver l'index du logement dans le tableau de logements
-    const index = findIndexByKeyValue(houses, "id", params);
-    // Récupération uniquement des informations propres au logement
-    const house = houses[index];
-
-    return (
-      <section className="housepage">
-      <Header />
-      <Gallery pictures={house.pictures}/>
-        <article className="locAndHost">
-          <HouseDetails
-            title={house.title}
-            location={house.location}
-            tags={house.tags}
-          />
-          <article className="host">
-            <article>
-              <HostProfile
-                name={house.host.name}
-                picture={house.host.picture}
-              />
+    if (this.state.house != null) {
+      return (
+        <section className="housepage">
+          <Header />
+          <Gallery pictures={this.state.house.pictures} />
+          <article className="locAndHost">
+            <HouseDetails
+              title={this.state.house.title}
+              location={this.state.house.location}
+              tags={this.state.house.tags}
+            />
+            <article className="host">
+              <article>
+                <HostProfile
+                  name={this.state.house.host.name}
+                  picture={this.state.house.host.picture}
+                />
+              </article>
+              <Rating rating={this.state.house.rating} />
             </article>
-            <Rating rating={house.rating} />
           </article>
-        </article>
-        <section className="blocDescription">
-          <Collapse title="Description" description={house.description}/>
-          <Collapse
-            title="Equipements"
-            description={house.equipments.map((equipment, index) => (
-              <li key={`${equipment}-${index}`}>{equipment}</li>
-            ))}
-          />
+          <section className="blocDescription">
+            <Collapse
+              title="Description"
+              description={this.state.house.description}
+            />
+            <Collapse
+              title="Equipements"
+              description={this.state.house.equipments.map(
+                (equipment, index) => (
+                  <li key={`${equipment}-${index}`}>{equipment}</li>
+                )
+              )}
+            />
+          </section>
+          <Footer />
         </section>
-        <Footer />
-      </section>
-    );
+      );
+    } else {
+      return(
+        <div>Error</div>
+      )
+    }
   }
 }
 
